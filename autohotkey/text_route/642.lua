6f706f78ee3d4c97cfbc2f569ccb9a3fabf7536d
liukangjian <liukangjian@bsmail.com>
2024-09-07 17:12:43 +0800

    642

--- autohotkey/text_route/642.lua
+++ autohotkey/text_route/642.lua
@@ -6,18 +6,18 @@ monster: [10, 2], pos: [35700, 49094]
 monster: [10, 2], pos: [35700, 49094]
 monster: [10, 2], pos: [39234, 58294]
 monster: [10, 2], pos: [39234, 58294]
-monster: [7, 1], pos: [6428, 35887], wheel: 10
+monster: [7, 1], pos: [7426, 35818], narrow: 1
 monster: [7, 1], pos: [3176, 48730]
-monster: [7, 1], pos: [31551, 35022], select: 2
+monster: [7, 1], pos: [31103, 35940], narrow: -1
 monster: [7, 1], pos: [20436, 58430]
-monster: [8, 2], pos: [32473, 35022], select: 2
+monster: [8, 2], pos: [31871, 40766], narrow: -3
 monster: [8, 2], pos: [11806, 41717]
 monster: [7, 3], pos: [26864, 2915]
-monster: [7, 3], pos: [36903, 1275], wheel: 12
+monster: [7, 3], pos: [36788, 577], narrow: 1
 monster: [8, 1], pos: [40591, 14756]
 monster: [7, 2], pos: [22255, 35887]
 monster: [7, 2], pos: [40719, 13617]
-monster: [7, 2], pos: [41923, 2049]
+monster: [7, 2], pos: [39843, 8863], narrow: 1
 monster: [3, 2], pos: [42870, 63804]
 monster: [3, 2], pos: [37646, 43720]
 monster: [3, 2], pos: [33753, 7332], select: 2
@@ -30,14 +30,14 @@ monster: [3, 3], pos: [23843, 32153], try: 2
 monster: [3, 3], pos: [29707, 50461]
 monster: [3, 3], pos: [31295, 45360]
 monster: [3, 3], pos: [43152, 45269]
-monster: [6, 1], pos: [19284, 12387], fastWheel: 30
+monster: [6, 1], pos: [19284, 12387]
 monster: [6, 1], pos: [52372, 14847]
 monster: [6, 1], pos: [51322, 34657]
-monster: [6, 1], pos: [28017, 61300], wheel: 10
-monster: [6, 2], pos: [35982, 16076], qm: false
-monster: [6, 2], pos: [47378, 19811], qm: false
+monster: [6, 1], pos: [28167, 60466], narrow: 1
+monster: [6, 2], pos: [35982, 16076]
+monster: [6, 2], pos: [47378, 19811]
 monster: [6, 2], pos: [36212, 30331], waitBook: 120
 monster: [6, 2], pos: [43690, 33792]
 monster: [6, 2], pos: [55112, 45314]
-monster: [6, 2], pos: [47531, 54742], wheel: 20
+monster: [6, 2], pos: [49386, 57673], narrow: 1
 monster: [6, 2], pos: [38952, 54605]
