a6e140ce3057435e70e396258f0d27af23336a83
lanlala <1700708766@qq.com>
2024-11-06 18:56:57 +0800

    添加5.1.2版本骗骗花路线

---
+++ autohotkey/text_route/小怪_骗骗花5.1.lua
@@ -0,0 +1,32 @@
+monster: [9, 2], pos: [54299, 27817], try: 1, name:"起点" -- 枫丹
+monster: [9, 2], pos: [43644, 16156], try: 1, name:"秋风山东侧"
+
+monster: [3, 1], pos: [40468, 50047], try: 1, name:"天衡山" --璃月
+monster: [6, 3], pos: [47777, 64017], try: 1, name:"琉璃峰"
+monster: [6, 3], pos: [42244, 58004], try: 1, name:"地面矿区"
+
+monster: [6, 3], pos: [30736, 57275], try: 1, name:"草本" --须弥
+monster: [7, 2], pos: [20934, 19800], try: 1, name:"维摩庄"
+monster: [7, 1], pos: [36712, 48832], try: 1, name:"觉王之殿"
+monster: [8, 2], pos: [28208, 31158], try: 1, name:"亡者狭廊"
+monster: [6, 3], pos: [26398, 28364], try: 1, name:"无郁稠林"
+
+monster: [3, 1], pos: [28243, 32919], try: 1, name:"天遒谷" --璃月
+monster: [3, 1], pos: [35346, 28789], try: 1, name:"爆炎树"
+monster: [3, 1], pos: [35346, 28789], try: 1, name:"爆炎树" --重复传爆炎树
+monster: [2, 3], pos: [22437, 61952], try: 1, name:"荻花洲"
+monster: [10, 3], pos: [30326, 25024], try: 1, name:"沉玉谷"
+
+monster: [4, 3], pos: [31863, 60008], try: 1, name:"" --稻妻
+monster: [4, 2], pos: [56485, 31097], try: 1, name:""
+monster: [4, 2], pos: [35209, 57396], try: 1, name:""
+monster: [4, 2], pos: [29028, 56060], try: 1, name:""
+monster: [4, 1], pos: [29028, 35652], try: 1, narrow: -1, name:""
+monster: [5, 1], pos: [28140, 33162], try: 1, name:""
+monster: [5, 1], pos: [27935, 50958], try: 1, name:""
+monster: [5, 3], pos: [35995, 7592], try: 1, name:""
+monster: [5, 2], pos: [19944, 31948], try: 1, name:""
+
+monster: [6, 1], pos: [24998, 54602], try: 1, name:"" --渊下宫
+monster: [6, 1], pos: [5396, 46281], try: 1, narrow: 3, name:""
+
